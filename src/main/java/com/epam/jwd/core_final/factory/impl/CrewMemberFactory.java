67f44abdfc5b14f0a07f6252a6a6da8e3f5dc051
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2020-11-05 19:04:11 +0300

    Factories implementations

--- src/main/java/com/epam/jwd/core_final/factory/impl/CrewMemberFactory.java
+++ src/main/java/com/epam/jwd/core_final/factory/impl/CrewMemberFactory.java
@@ -1,13 +1,28 @@
 package com.epam.jwd.core_final.factory.impl;
 
+import com.epam.jwd.core_final.context.Application;
 import com.epam.jwd.core_final.domain.CrewMember;
+import com.epam.jwd.core_final.domain.Rank;
+import com.epam.jwd.core_final.domain.Role;
+import com.epam.jwd.core_final.exception.InvalidStateException;
 import com.epam.jwd.core_final.factory.EntityFactory;
 
 // do the same for other entities
 public class CrewMemberFactory implements EntityFactory<CrewMember> {
 
+    private CrewMemberFactory() {
+
+    }
+
+    private final static CrewMemberFactory INSTANCE = new CrewMemberFactory();
+
+    public static CrewMemberFactory getInstance() {
+        return INSTANCE;
+    }
+
     @Override
     public CrewMember create(Object... args) {
-        return null;
+        return new CrewMember((Role)args[0], (String) args[1], (Rank)args[2]);
     }
+
 }
