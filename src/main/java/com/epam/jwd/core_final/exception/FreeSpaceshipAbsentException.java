652a1465128c8ac8950f0d1d4b16a09973628023
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2020-11-08 16:01:58 +0300

    Add exception for cases when all spaceships are taken

---
+++ src/main/java/com/epam/jwd/core_final/exception/FreeSpaceshipAbsentException.java
@@ -0,0 +1,8 @@
+package com.epam.jwd.core_final.exception;
+
+public class FreeSpaceshipAbsentException extends Exception{
+
+    public FreeSpaceshipAbsentException(String message) {
+        super(message);
+    }
+}
