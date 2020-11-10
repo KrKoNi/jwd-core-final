cdba7b2a0157f37703880dd57a6803b19f82d99a
Kanstantsin Kres <38403018+KrKoNi@users.noreply.github.com>
2020-11-10 21:14:01 +0300

    End Date is Before Start Date Exception

---
+++ src/main/java/com/epam/jwd/core_final/exception/EndDateIsBeforeStartDateException.java
@@ -0,0 +1,7 @@
+package com.epam.jwd.core_final.exception;
+
+public class EndDateIsBeforeStartDateException extends Exception{
+    public EndDateIsBeforeStartDateException(String message) {
+        super(message);
+    }
+}
